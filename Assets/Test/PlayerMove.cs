0ef75b19d1b4761ee005bcddced7e3c497786bec
20181181송영훈 <83389557+songyeonghun@users.noreply.github.com>
2023-04-17 02:50:22 +0900

    플레이어 정돈
    코드좀 깔끔하게 함

--- Assets/Test/PlayerMove.cs
+++ Assets/Test/PlayerMove.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-
     public Rigidbody2D rb;
     Vector2 movement;
+    Animator anim;
+    public GameObject emp;
+
+    float moveSpeed;
 
     //대쉬
     bool canDash = true;
-    float DashTime = 0.3f;
-    float dashCoolTime = 0.5f;
+    float dashTime = 0.3f;
+
 
     void Start()
     {
-        
+        moveSpeed = Player.stat[2][PlayerPrefs.GetInt("statMoveSpeed")]/2;
     }
 
     void Update()
@@ -27,25 +30,29 @@ public class PlayerMove : MonoBehaviour
         //마우스 우클릭시 대쉬
         if (Input.GetMouseButtonDown(1) && canDash == true)
             StartCoroutine(Dash());
+        //emp
+        if (Input.GetKeyDown(KeyCode.Space) && Player.emp >= 1)
+        {
+            Instantiate(emp, gameObject.transform.position, Quaternion.identity);
+            Player.emp--;
+        }
     }
 
     void FixedUpdate()
     {
         //rb를 이용한 물리적 플레이어 이동
-        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
     //대쉬
     private IEnumerator Dash()
     {
         canDash = false;
-       // DashSpeed = 1;
-        //대쉬시 무적
         gameObject.layer = 0;
-        yield return new WaitForSeconds(DashTime);
-      //  DashSpeed = 2;
-        gameObject.layer = 10;
-        yield return new WaitForSeconds(dashCoolTime);
+        moveSpeed = moveSpeed * 1.5f;
+        yield return new WaitForSeconds(dashTime);
         canDash = true;
+        gameObject.layer = 0;
+        moveSpeed = Player.stat[2][PlayerPrefs.GetInt("statMoveSpeed")]/2;
     }
 }
