c20fa99a00261e21c0ab2479e94883d6339d1628
20181185신준원 <wnsdnjs81@gmail.com>
2023-05-28 01:52:17 +0900

    AA만드는중
    0

---
+++ Assets/Script/Enemy/BatteryAA.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryAA : MonoBehaviour
+{
+    public GameObject Body;
+
+    public int Hp=10;
+    int Damage;
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+                Bullet BulletDamage = collision.GetComponent<Bullet>();
+                Hp -= BulletDamage.damage;
+
+            Destroy(collision.gameObject);
+
+            if (Hp <= 0)
+            {
+                Destroy(Body);
+            }
+        }
+    }
+
+
+}
--- Assets/Script/Enemy/BatteryBullet.cs
+++ Assets/Script/Enemy/BatteryBullet.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBullet : MonoBehaviour
+public class BatteryBullet : MonoBehaviour
 {
+    public int Damage = 10;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
-            Player.CurrentHp -= 10; //데미지를 몬스터의 데미지를 가져올것
+            Player.CurrentHp -= Damage; //데미지를 몬스터의 데미지를 가져올것
 
             Destroy(this.gameObject);     
     }
