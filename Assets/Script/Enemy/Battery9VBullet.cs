1689425862dff7a65e9a05a7edd878741c645d67
20181185신준원 <wnsdnjs81@gmail.com>
2023-05-28 00:57:32 +0900

    임시저장
    ㅇㅇ

--- Assets/Script/Enemy/Battery9VBullet.cs
+++ Assets/Script/Enemy/Battery9VBullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Battery9VBullet : MonoBehaviour
+public class EnemyBullet : MonoBehaviour
 {
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -10,6 +10,7 @@ public class Battery9VBullet : MonoBehaviour
         if (collision.gameObject.tag == "Player")
             Player.CurrentHp -= 10; //데미지를 몬스터의 데미지를 가져올것
 
-        Destroy(this.gameObject);
+            Destroy(this.gameObject);     
     }
+
 }
